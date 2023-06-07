6f309d90d3869cfc6c6f7c7814ecdc818bb47116
mohamedelewasy <eng.m.elewasy@gmail.com>
2023-06-07 17:24:04 +0300

    featured products

--- e-commerce-frontend/ts/types/api.ts
+++ e-commerce-frontend/ts/types/api.ts
@@ -1,6 +1,27 @@
 export type loginReq = { email: string; password: string };
 export type loginRes = { token: string };
 
+export interface Icategory {
+  _id: string;
+  name: string;
+  image: string;
+  productCount: number;
+}
+
+export interface Iproduct {
+  _id: string;
+  name: string;
+  image: string;
+  category_id: string;
+  price: number;
+  discount: number;
+  rating: number;
+  rating_count: number;
+  is_featured: boolean;
+  is_recent: boolean;
+  description: string;
+}
+
 export type registerReq = {
   first_name: string;
   last_name: string;
@@ -11,14 +32,8 @@ export type registerRes = {
   token: string;
 };
 
-export interface Icategory {
-  _id: string;
-  name: string;
-  image: string;
-  productCount: number;
-}
-
 export type getCategoriesReq = undefined;
-export type getCategoriesRes = {
-  data: [Icategory];
-};
+export type getCategoriesRes = { data: [Icategory] };
+
+export type getFeaturedReq = undefined;
+export type getFeaturedRes = { data: [Iproduct] };
